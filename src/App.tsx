ba5c17e218fad5b8d90b8a45f7fa1f9f5c27a564
GianDutra <chikegang@gmail.com>
2023-07-04 14:55:41 -0300

    trocando meu projeto Ignite Feed javascript (repositório deste também está no meu github) para o Ignite Feed versão typescript

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from './components/Sidebar';
+
+const posts = [
+  { 
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/kevinsilvadev.png',
+      name: 'Kevin Silva',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!' },
+      { type: 'paragraph', content: 'Acabei de ver o portafólio do Gian Dutra, ficou incrível... parabéns!!!' },
+      { type: 'link', content: 'meu-portfolio-giandutra.vercel.app' },
+    ], publishedAt: new Date('2023-06-28 20:00:00'),
+  },
+  { 
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Opa pessoal!' },
+      { type: 'paragraph', content: 'Acabei de ver o vídeo publicado pelo Gian sobre o Vtuber Voice, gostei bastante....' },
+      { type: 'link', content: 'www.youtube.com/watch?v=43CvhUgiKCs' },
+    ], publishedAt: new Date('2023-06-25 20:00:00'),
+  }
+];
+
+
+export function App() {
+  return (
+    <div>
+
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        {posts.map(post => (
+    <Post
+      key = {post.id}
+      author={post.author}
+      content={post.content}
+      publishedAt={post.publishedAt}
+    />
+  ))}
+        </main>
+      </div>
+    </div>
+  );
+}
+
